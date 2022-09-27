778acffe7984aa09fe8cc269ceb82df25ceb0f57
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2022-09-27 12:24:15 -0300

    Add files via upload

---
+++ Urna/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Urna</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>header</h1>
+    </header>
+    <main>
+        <div class="urna">
+            <div class="urna__tela">
+            </div>
+            <aside class="urna__lado">
+                <div class="urna__titulo">
+                    <h1>Justiça Eleitoral</h1>
+                </div>
+                <div class="urna__numeros">
+                    <div class="numero"><a href="#">1</a></div>
+                    <div class="numero"><a href="#">2</a></div>
+                    <div class="numero"><a href="#">3</a></div>
+                    <div class="numero"><a href="#">4</a></div>
+                    <div class="numero"><a href="#">5</a></div>
+                    <div class="numero"><a href="#">6</a></div>
+                    <div class="numero"><a href="#">7</a></div>
+                    <div class="numero"><a href="#">8</a></div>
+                    <div class="numero"><a href="#">9</a></div>
+                    <div class="numero"><a href="#">0</a></div>
+                </div>
+                <div class="urna__teclas">
+                    <div class="tecla--branco"><a href="#">Branco</a></div>
+                    <div class="tecla--vermelho"><a href="#">Corrige</a></div>
+                    <div class="tecla--verde"><a href="#">Confirma</a></div>
+                </div>
+            </aside>
+        </div>
+    </main>
+</body>
+</html>
