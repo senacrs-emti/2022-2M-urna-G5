fff037ddacef0380763fcd2f3b071089d7f49ae2
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2022-09-29 11:59:07 -0300

    Add files via upload

---
+++ 2022-2M-urna-G5-main/2022-2M-urna-G5-main/Urna/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Urna</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>header</h1>
+    </header>
+    <main>
+        <div class="urna">
+            <div class="urna__tela">
+                <div class="tela__cargo">
+                    <h1 class="deputadoFederal">Deputado Federal</h1>
+                    <h1 class="deputadoEstadual">Deputado Estadual</h1>
+                    <h1 class="senador">Senador</h1>
+                    <h1 class="governador">Governador</h1>
+                    <h1 class="presidente">Presidente</h1>
+                </div>
+                <div class="tela__digitos">
+                    <div class="tela__digito digito1"><h1></h1></div>
+                    <div class="tela__digito digito2"><h1></h1></div>
+                    <div class="tela__digito digito3"><h1></h1></div>
+                    <div class="tela__digito digito4"><h1></h1></div>
+                    <div class="tela__digito digito5"><h1></h1></div>
+                    <div class="tela__digito digito6"><h1></h1></div>
+                    <div class="tela__digito digito7"><h1></h1></div>
+                    <div class="tela__digito digito8"><h1></h1></div>
+                    <div class="tela__digito digito9"><h1></h1></div>
+                    <div class="tela__digito digito10"><h1></h1></div>
+                    <div class="tela__digito digito11"><h1></h1></div>
+                    <div class="tela__digito digito12"><h1></h1></div>
+                    <div class="tela__digito digito13"><h1></h1></div>
+                    <div class="tela__digito digito14"><h1></h1></div>
+                    
+                </div>
+            </div>
+            <aside class="urna__lado">
+                <div class="urna__titulo">
+                    <h1>Justiça Eleitoral</h1>
+                </div>
+                <div class="urna__numeros">
+                    <div class="numero"><h1>1</h1></div>
+                    <div class="numero"><h1>2</h1></div>
+                    <div class="numero"><h1>3</h1></div>
+                    <div class="numero"><h1>4</h1></div>
+                    <div class="numero"><h1>5</h1></div>
+                    <div class="numero"><h1>6</h1></div>
+                    <div class="numero"><h1>7</h1></div>
+                    <div class="numero"><h1>8</h1></div>
+                    <div class="numero"><h1>9</h1></div>
+                    <div class="numero"><h1>0</h1></div>
+                </div>
+                <div class="urna__teclas">
+                    <div class="tecla--branco">Branco</div>
+                    <div class="tecla--vermelho">Corrige</div>
+                    <div class="tecla--verde">Confirma</div>
+                </div>
+            </aside>
+        </div>
+    </main>
+    <script src="js/script.js"></script>
+</body>
+</html>
